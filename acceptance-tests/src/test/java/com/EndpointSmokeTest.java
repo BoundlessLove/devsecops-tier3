4bf8fb56222626ff81444e728ace6555b28026ab
jyotirmay <webmaster@systematicdefence.tech>
2026-04-22 03:45:46 +1200

    updated pom.xml

--- acceptance-tests/src/test/java/com/EndpointSmokeTest.java
+++ acceptance-tests/src/test/java/com/EndpointSmokeTest.java
@@ -1,10 +1,27 @@
-package com;
+/*package com;
 
 import org.concordion.integration.junit5.ConcordionExtension;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.apache.hc.client5.http.fluent.Request;
 
 @ExtendWith(ConcordionExtension.class)
+public class EndpointSmokeTest {
+
+    public String callEndpoint() throws Exception {
+        String ip = System.getProperty("externalIp");
+        return Request.get("http://" + ip + "/")
+                .execute()
+                .returnContent()
+                .asString();
+    }
+}*/
+package com;
+
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+import org.apache.hc.client5.http.fluent.Request;
+
+@RunWith(ConcordionRunner.class)
 public class EndpointSmokeTest {
 
     public String callEndpoint() throws Exception {
