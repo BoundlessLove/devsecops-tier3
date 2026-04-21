13e6c1f66e00aaf423735cb89e57702c5f51083c
jyotirmay <webmaster@systematicdefence.tech>
2026-04-22 01:42:19 +1200

    Acceptance driven test driven development

---
+++ acceptance-tests/src/test/java/com/EndpointSmokeTest.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.concordion.integration.junit5.ConcordionExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.apache.hc.client5.http.fluent.Request;
+
+@ExtendWith(ConcordionExtension.class)
+public class EndpointTest {
+
+    public String callEndpoint() throws Exception {
+        String ip = System.getProperty("externalIp");
+        return Request.get("http://" + ip + "/")
+                .execute()
+                .returnContent()
+                .asString();
+    }
+}
