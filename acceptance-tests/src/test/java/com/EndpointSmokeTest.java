44bdb202ec14fb0832ae2ad1d3aae286110ade29
jyotirmay <webmaster@systematicdefence.tech>
2026-04-22 02:51:31 +1200

    fixed pom.xl to include necessary dependancies and temporarily removed the teardown step

--- acceptance-tests/src/test/java/com/EndpointSmokeTest.java
+++ acceptance-tests/src/test/java/com/EndpointSmokeTest.java
@@ -1,11 +1,11 @@
-package com.example;
+package com;
 
 import org.concordion.integration.junit5.ConcordionExtension;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.apache.hc.client5.http.fluent.Request;
 
 @ExtendWith(ConcordionExtension.class)
-public class EndpointTest {
+public class EndpointSmokeTest {
 
     public String callEndpoint() throws Exception {
         String ip = System.getProperty("externalIp");
